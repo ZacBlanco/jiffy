a39fb29f5c1c553fc0b9e40e66e3a591a03bfb09
zblanco <zblanco@apache.org>
2015-10-07 17:11:50 -0400

    Added document spec and enum for document types

---
+++ jiffy-web/src/main/java/com/blancoz/jiffy/web/Document.java
@@ -0,0 +1,51 @@
+package com.blancoz.jiffy.web;
+
+import java.util.HashMap;
+
+/**
+ *   This class provides some basic properties and functions that
+ *   every document should have.
+ */
+public abstract class Document {
+
+  private final DocumentType type;
+  protected String name;
+
+
+  // Static map for file extensions
+  protected static final HashMap<DocumentType, String> fileExtensions;
+  static  {
+    fileExtensions = new HashMap<DocumentType, String>();
+    fileExtensions.put(DocumentType.HTML_DOCUMENT, ".html");
+    fileExtensions.put(DocumentType.CSS_STYLESHEET, ".css");
+    fileExtensions.put(DocumentType.JAVASCRIPT, ".js");
+  }
+
+  public Document() {
+    this(null);
+  }
+
+  public Document(String name) {
+    this(name, null);
+  }
+
+  public Document(String name, DocumentType type) {
+    this.name = name;
+    this.type = type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getFileName() {
+    return name;
+  }
+
+  public DocumentType getDocumentType() {
+    return type;
+  }
+
+  public abstract String toString();
+  public abstract void writeToFile(String location);
+}
---
+++ jiffy-web/src/main/java/com/blancoz/jiffy/web/DocumentType.java
@@ -0,0 +1,7 @@
+package com.blancoz.jiffy.web;
+
+public enum DocumentType {
+    HTML_DOCUMENT,
+    CSS_STYLESHEET,
+    JAVASCRIPT
+}
