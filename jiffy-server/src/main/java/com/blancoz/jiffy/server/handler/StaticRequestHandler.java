e0978a11b8147b04db32c754bae39c2b590af9b3
zblanco <zblanco@apache.org>
2015-10-07 23:01:34 -0400

    https://github.com/ZacBlanco/jiffy/issues/11

---
+++ jiffy-server/src/main/java/com/blancoz/jiffy/server/handler/StaticRequestHandler.java
@@ -0,0 +1,46 @@
+package com.blancoz.jiffy.server.handler;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import com.blancoz.jiffy.web.html.HtmlDocument;
+
+public class StaticRequestHandler extends RequestHandler {
+
+  private HtmlDocument document;
+
+  public StaticRequestHandler(HtmlDocument document) {
+    this.document = document;
+  }
+
+  public void setDocument(HtmlDocument d) {
+    if(d != null) {
+      this.document = d;
+    }
+  }
+
+  public HtmlDocument getDocument() {
+    return this.document;
+  }
+  @Override
+  protected void get(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.getWriter().write(document.toString());
+  }
+
+  @Override
+  protected void put(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    return;
+  }
+
+  @Override
+  protected void post(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    return;
+  }
+
+  @Override
+  protected void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    return;
+  }
+}
