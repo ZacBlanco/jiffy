2ad88fed65f6b6ab556c4acaa8f50d95391ec9d3
zblanco <zblanco@apache.org>
2015-10-13 23:52:01 -0400

    Update for Jiffy Config to address #2. Still need to add a readFile method for configuration files.

--- jiffy-server/src/main/java/com/blancoz/jiffy/server/config/JiffyConfig.java
+++ jiffy-server/src/main/java/com/blancoz/jiffy/server/config/JiffyConfig.java
@@ -1,23 +1,43 @@
 package com.blancoz.jiffy.server.config;
 
-import java.util.HashMap;
+import java.util.Properties;
 
 public class JiffyConfig {
 
-  private HashMap<String, String> properties = new HashMap<String, String>();
+  static final String PROPERTY_PORT = "jiffy.port";
+  static final int MAX_PORT = 65535;
+  static final int MIN_PORT = 1025;
 
-  public int getPort() {
-    return Integer.parseInt(properties.get("port"));
+  private Properties propList = new Properties();
+
+  static final int JIFFY_DEFAULT_PORT = 7090;
+
+  private String getProperty(String prop) throws NullPointerException {
+    return System.getProperty(prop);
   }
 
-  public void setPort(int port) {
-    if(port > 0) {
-      properties.put("port", port + "");
+  private void setProperty(String property, String value) throws NullPointerException {
+    if(property != null && value != null) {
+      propList.setProperty(property, value);
+    } else if(property == null) {
+      throw new NullPointerException("Property cannot be null");
+    } else {
+      throw new NullPointerException("Property value cannot be null");
     }
   }
 
-  public void setProperty(String key, String value) {
-    properties.put(key, value);
+  public int getPort() {
+    String port = getProperty(PROPERTY_PORT);
+    return Integer.parseInt(port);
   }
+
+  public void setPort(int port) {
+    if(port <= MAX_PORT && port >= MIN_PORT) {
+      propList.setProperty(PROPERTY_PORT, port + "");
+    } else {
+      throw new IllegalArgumentException("Port must be between: " + MIN_PORT + " and " + MAX_PORT);
+    }
+  }
+
 
 }
